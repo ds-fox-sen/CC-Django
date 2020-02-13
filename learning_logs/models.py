d1cbbd9fd4f655c79de89e2f675ac27fb1213a50
fox-sen <xr223r@gmail.com>
2020-02-13 23:25:15 +0700

    Add Topic View
    Create a view for individual topic, add template and urls.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -24,7 +24,7 @@ class Topic(models.Model):
 class Entry(models.Model):
     """Something learned about a topic."""
 
-    topic = models.ForeignKey(Topic, on_delete=models.DO_NOTHING)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
